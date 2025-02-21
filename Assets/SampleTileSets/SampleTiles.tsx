dc02c1076e30157b745630e41d2f0ddac64d3501
S Hemamalini <hemamalini.programmer@gmail.com>
2025-02-22 00:24:53 +0530

    Level Design Mockup and Sample Tile Map

---
+++ Assets/SampleTileSets/SampleTiles.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="SampleTiles" tilewidth="48" tileheight="48" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="8">
+  <image source="Tiles/tile15.png" width="48" height="48"/>
+ </tile>
+ <tile id="12">
+  <image source="Tiles/tile118.png" width="48" height="48"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="48" height="48"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <image source="Tiles/tile28.png" width="48" height="48"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="48" height="48"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <image source="Tiles/tile11.png" width="48" height="48"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="48" height="48"/>
+  </objectgroup>
+ </tile>
+</tileset>
